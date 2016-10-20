7bc6e085ec1b38050c0aaf03cd3bd8c22130ed41
Pauline Sarana <p_louie_s@yahoo.com>
2016-10-20 09:37:26 +0800

    Added Mobile files, Added CompMsg folder

---
+++ Mobile/Comp Msg/main.lua
@@ -0,0 +1,12 @@
+
+local composer = require( "composer" )
+
+display.setStatusBar( display.HiddenStatusBar )
+
+math.randomseed( os.time() )
+-- Configure image sheet
+
+	
+composer.gotoScene( "comp" )
+	
+	
