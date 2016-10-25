e51753f772bdceceb87875bf0221ee707573c698
Pauline Sarana <p_louie_s@yahoo.com>
2016-10-25 17:01:42 +0800

    Edit for mobile demo

--- Mobile/MainFiles/creategroupchat.lua
+++ Mobile/MainFiles/creategroupchat.lua
@@ -21,8 +21,8 @@ local function gotoCreateGroup()
 			print ( "RESPONSE: " .. event.response )
 		end
 	end
-	-- network.request( "http://192.168.43.114:8080/studybuddies/groupchat/insert/"..gnameni.."/"..uid, "GET", networkListener)
-	network.request( "http://localhost:8080/studybuddies/groupchat/insert/"..gnameni.."/"..uid, "GET", networkListener)
+	network.request( "http://192.168.43.114:8080/studybuddies/groupchat/insert/"..gnameni.."/"..uid, "GET", networkListener)
+	-- network.request( "http://localhost:8080/studybuddies/groupchat/insert/"..gnameni.."/"..uid, "GET", networkListener)
 
 	local options = {
 		effect = "crossFade",
